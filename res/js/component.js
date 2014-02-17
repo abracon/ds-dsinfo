0015d65c3f15cded85ce19faaecc71ebf6fcc363
dat <dat@dbd45110-ba1d-0410-af47-983eeb39c420>
2014-02-17 17:34:31 +0000

    initial commit
    git-svn-id: http://abrasvrex.abracon.local:8080/svn/openbi/design-studio-addons/de.abracon.dsinfo/trunk@7904 dbd45110-ba1d-0410-af47-983eeb39c420

---
+++ res/js/component.js
@@ -0,0 +1,57 @@
+sap.designstudio.sdk.Component.subclass("de.abracon.dsinfo.DSInfo", function () {
+
+    // Private members: initialize them as you can not be sure that their setters
+    // are called before they are accessed for the first time.
+    var _resultSet = {
+        dimensions : [],
+        axis_rows : []
+    };
+
+    // Executed after the extension component's handler element has been created.
+    this.init = function () {
+    };
+
+    // Executed before the properties of the extension component are about to be updated.
+    this.beforeUpdate = function () {
+    };
+
+    // Executed each time after all properties of the custom extension component
+    // have been updated.
+    this.afterUpdate = function () {
+//        // Get the number of rows in the result set.
+//        var $this = this.$();
+//
+//        $this.empty();
+//        $this.html('Data Source Info: ' + '<br>' + this.numberOfRows() + ' rows');
+//
+//        // Only display the component in design mode. 
+//        if (!sap.zen.designmode) {
+//            $this.css('display', 'none');
+//        }
+
+        // TODO: firePropertiesChanged will make the run time trigger this method once more..
+        this.firePropertiesChanged(["numberOfRows", "dimensions"]);
+        this.fireEvent('onvaluechange');
+    };
+
+    this.numberOfRows = function () {
+        return _resultSet.axis_rows && _resultSet.axis_rows.length || 0;
+    };
+
+    this.dimensions = function () {
+        return _resultSet.dimensions && JSON.stringify(_resultSet.dimensions) || '{}';
+    };
+
+    this.resultSet = function (resultSet) {
+        if (resultSet === undefined) {
+            return _resultSet;
+        } else {
+            _resultSet = resultSet || {
+                dimensions : [],
+                axis_rows : []
+            };
+            _updateIndicator = true;
+            return this;
+        }
+    };
+});
